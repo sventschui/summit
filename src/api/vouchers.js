fa14026bd0074c6888cf0868db47eaf73adefd5a
Sven Tschui <github@sventschui.ch>
2015-12-17 10:05:49 +0100

    Store vouchers in a const

--- src/api/vouchers.js
+++ src/api/vouchers.js
@@ -4,18 +4,20 @@ import notifier from 'node-notifier';
 
 const vouchers = express();
 
+const vouch = {
+  "Drinks": 10,
+  "Fishing": 50,
+  "Weapons": 100,
+  "Phones": 100,
+  "Computers": 200,
+  "Cars": 1000,
+  "Planes": 20000,
+  "Houses": 20000,
+  "Boats": 20000,
+};
+
 vouchers.get('/', (req, res) => {
-  res.send({
-    "Drinks": 10,
-    "Fishing": 50,
-    "Weapons": 100,
-    "Phones": 100,
-    "Computers": 200,
-    "Cars": 1000,
-    "Planes": 20000,
-    "Houses": 20000,
-    "Boats": 20000,
-  });
+  res.send(vouch);
   res.end();
 });
 
